1aab189d54d54d408b27918a8848740863139046
BayaSea <bayasea@iMac.local>
2020-04-26 02:37:33 +0900

    annotate実行

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id          :bigint           not null, primary key
+#  description :text(65535)
+#  name        :string(255)      not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_categories_on_name  (name) UNIQUE
+#
 class Category < ApplicationRecord
 end
--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: issues
+#
+#  id          :bigint           not null, primary key
+#  text        :text(65535)      not null
+#  title       :string(255)      not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  category_id :bigint           not null
+#
+# Indexes
+#
+#  index_issues_on_category_id  (category_id)
+#
 class Issue < ApplicationRecord
   belongs_to :category
 end
